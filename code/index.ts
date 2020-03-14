c48fc88f1ff76116ceb8e81cfe2ef9cf38220bf3
Lau Skeeter <hi@laura.monster>
2020-03-14 19:47:37 +0000

    Update index.ts

--- code/index.ts
+++ code/index.ts
@@ -1,17 +1,19 @@
 import { routes } from './help/routes';
 
-let getRoute: Promise<any> = null;
+let getRoute: () => Promise<any> = null;
 Object.entries(routes).forEach(([route, callback]) => {
 	if (document.location.search.includes(route)) {
 		console.log('using route: ' + route);
-		getRoute = callback();
+		getRoute = callback;
 	}
 });
 
 if (!getRoute) {
 	const [useName, useCb] = Object.entries(routes).shift();
 	console.log('using default route: ' + useName);
-	getRoute = useCb();
+	getRoute = useCb;
 }
 
-getRoute.then((r) => r());
+getRoute().then((r) => {
+	r.default();
+});
